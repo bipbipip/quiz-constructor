a2d9cc69da7c758ff13e5be78bd0b8a3cdef89e9
bipbipip <ilyashereshik@mail.ru>
2025-07-03 19:06:37 +0300

    add render to other pages

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import {setItem, getItem, removeItem, clearStorage, updateItem} from "./storage.
 
 import {processPage} from "./pages.js";
 import {renderQuizList} from "./utils/render/renderQuizes.js";
+import {renderCreateQuiz} from "./utils/render/renderCreateQuiz.js";
 
 
 
---
+++ utils/render/renderHome.js
@@ -0,0 +1,14 @@
+import {header} from "../../components/header/header.js";
+import {render} from "./render.js";
+
+export function renderHomePage(app) {
+    header(app);
+    render(app, `
+        <div class="container">
+            <h1>Домашняя страница</h1>
+            <div class="create-quiz">
+                 
+            </div>
+        </div>
+    `);
+}
