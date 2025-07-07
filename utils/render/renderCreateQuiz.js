3e595c7af35e80178a021f67000fa8c84f5f0ed2
bipbipip <ilyashereshik@mail.ru>
2025-07-07 21:12:07 +0300

    create base of quiz

--- utils/render/renderCreateQuiz.js
+++ utils/render/renderCreateQuiz.js
@@ -8,8 +8,46 @@ export function renderCreateQuiz(app) {
         <div class="container">
             <h1>Создать тест</h1>
             <div class="create-quiz">
-                 
+                <form  method="get">
+                    <input type="text" id="key" name="quizName" placeholder="Название теста">
+                    <input type="text" id="description" name="quizDescription" placeholder="Описание теста">
+                    <select id="question-select" name="questions">
+                        <option value="single">Одиночный ответ</option>
+                        <option value="multi">Множественный ответ</option>
+                        <option value="detailed">Развернутый ответ</option>
+                    </select>
+                    <input type="submit" id="send" name="sendQuiz">
+                </form>
+                
             </div>
         </div>
     `);
 }
+setTimeout(() => {
+    const selectElement = document.getElementById("question-select");
+
+    if (selectElement) {
+
+
+        selectElement.addEventListener('change', function() {
+            const newValue = this.value;
+
+
+            switch (newValue) {
+                case 'single':
+                    console.log("Одиночный ответ");
+                    break;
+                case 'multi':
+                    console.log("Множественный ответ");
+                    break;
+                case 'detailed':
+                    console.log("Развернутый ответ");
+                    break;
+            }
+        });
+    }
+}, 0);
+
+
+
+
